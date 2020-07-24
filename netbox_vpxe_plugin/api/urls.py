ff135997ce9ee375e40e48a98cb34a9c08a837a9
Marco Ceppi <marco@ceppi.net>
2020-07-23 23:30:19 -0400

    :bug: Declare api/urls "app_name"

--- netbox_vpxe_plugin/api/urls.py
+++ netbox_vpxe_plugin/api/urls.py
@@ -11,4 +11,5 @@ router.register('images', BootImageViewSet)
 router.register('config', BootConfigViewSet)
 router.register('profile', BootProfileViewSet)
 
+app_name = 'netbox_vpxe_plugin-api'
 urlpatterns = router.urls
